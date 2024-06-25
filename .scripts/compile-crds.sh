46e2eabd0bd00d0fd84b3d82d7f0983625dc6eb6
Spyros Synodinos <spyros@giantswarm.io>
2024-06-25 17:42:58 +0300

    renovate addition

---
+++ .scripts/compile-crds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SRC_DIR="cert-manager/deploy/crds"
+DEST_FILE="templates/crds.yaml"
+
+# shellcheck disable=SC2046
+mkdir -p $(dirname "$DEST_FILE")
+
+# shellcheck disable=SC2188
+> "$DEST_FILE"
+
+
+for file in "$SRC_DIR"/*.yaml
+do
+    echo "---" >> "$DEST_FILE"
+    cat "$file" >> "$DEST_FILE"
+done
+
+echo "CRDs have been compiled into $DEST_FILE"
