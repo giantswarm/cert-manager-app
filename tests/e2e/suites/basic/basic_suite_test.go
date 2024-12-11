daada8e9c6852e7a7087a55dfa19bdb88ddfc933
Spyros Synodinos <spyros@giantswarm.io>
2024-12-11 14:54:02 +0200

    new kind of test

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -2,125 +2,280 @@ package basic
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"testing"
 	"time"
 
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmapiv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	"github.com/giantswarm/apptest-framework/pkg/config"
 	"github.com/giantswarm/apptest-framework/pkg/state"
 	"github.com/giantswarm/apptest-framework/pkg/suite"
-	"github.com/giantswarm/clustertest/pkg/wait"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	appsv1 "k8s.io/api/apps/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
-	isUpgrade        = false
-	appReadyTimeout  = 10 * time.Minute
-	appReadyInterval = 5 * time.Second
+	isUpgrade              = false
+	namespace              = "kube-system"
+	testTimeout            = 10 * time.Minute
+	retryInterval          = 5 * time.Second
+	deploymentReadyTimeout = 5 * time.Minute
 )
-
-var components = []string{
-	"cert-manager",
-}
 
 func TestBasic(t *testing.T) {
 	suite.New(config.MustLoad("../../config.yaml")).
+		WithInstallNamespace(namespace).
 		WithIsUpgrade(isUpgrade).
 		WithValuesFile("./values.yaml").
 		Tests(func() {
-			It("should deploy cert-manager App CR successfully", func() {
-				Expect(state.GetCluster()).NotTo(BeNil(), "cluster state should be initialized")
-				Expect(state.GetCluster().Organization).NotTo(BeNil(), "organization should be available")
-				namespace := state.GetCluster().Organization.GetNamespace()
+			It("should have CRDs installed", func() {
+				wcClient := state.GetFramework().MC()
 
-				By("Verifying cert-manager App CR is deployed")
-				for _, component := range components {
-					appName := fmt.Sprintf("%s-%s", state.GetCluster().Name, component)
-					Eventually(wait.IsAppDeployed(context.Background(),
-						state.GetFramework().MC(),
-						appName,
-						namespace)).
-						WithTimeout(appReadyTimeout).
-						WithPolling(appReadyInterval).
-						Should(BeTrue(), fmt.Sprintf("%s should be deployed", component))
+				crds := []string{
+					"certificates.cert-manager.io",
+					"certificaterequests.cert-manager.io",
+					"issuers.cert-manager.io",
+					"clusterissuers.cert-manager.io",
+					"orders.acme.cert-manager.io",
+					"challenges.acme.cert-manager.io",
+				}
+
+				for _, crdName := range crds {
+					By(fmt.Sprintf("Checking CRD %s", crdName))
+					Eventually(func() error {
+						crd := &apiextensionsv1.CustomResourceDefinition{}
+						err := wcClient.Get(context.Background(), types.NamespacedName{Name: crdName}, crd)
+						return err
+					}).
+						WithTimeout(testTimeout).
+						WithPolling(retryInterval).
+						ShouldNot(HaveOccurred())
 				}
 			})
 
-			It("should have all components running and ready", func() {
-				wcClient, err := state.GetFramework().WC(state.GetCluster().Name)
-				Expect(err).NotTo(HaveOccurred(), "should get workload cluster client")
+			It("should deploy cert-manager components", func() {
+				wcClient := state.GetFramework().MC()
 
-				componentConfigs := map[string]struct {
-					namespace string
-					kind      string
-					name      string
-				}{
-					// cert-manager namespace
-					"cert-manager-controller": {
-						namespace: "cert-manager",
-						kind:      "Deployment",
-						name:      "cert-manager",
-					},
-					"cert-manager-cainjector": {
-						namespace: "cert-manager",
-						kind:      "Deployment",
-						name:      "cert-manager-cainjector",
-					},
-					"cert-manager-webhook": {
-						namespace: "cert-manager",
-						kind:      "Deployment",
-						name:      "cert-manager-webhook",
-					},
-					// webhooks
-					"cert-manager-webhook-config": {
-						namespace: "",
-						kind:      "ValidatingWebhookConfiguration",
-						name:      "cert-manager-webhook",
-					},
-					"cert-manager-mutating-webhook-config": {
-						namespace: "",
-						kind:      "MutatingWebhookConfiguration",
-						name:      "cert-manager-webhook",
-					},
+				deployments := []string{
+					"cert-manager-app",
+					"cert-manager-app-webhook",
+					"cert-manager-app-cainjector",
 				}
 
-				for component, config := range componentConfigs {
-					By(fmt.Sprintf("Checking %s %s", component, config.kind))
-					Eventually(func() bool {
-						var ready, replicas int32
-
-						switch config.kind {
-						case "Deployment":
-							deployment := &appsv1.Deployment{}
-							err := wcClient.Get(context.Background(), client.ObjectKey{Namespace: config.namespace, Name: config.name}, deployment)
-							if err != nil {
-								return false
-							}
-							ready = deployment.Status.ReadyReplicas
-							replicas = deployment.Status.Replicas
-
-						case "MutatingWebhookConfiguration":
-							mutatingWebhook := &admissionregistrationv1.MutatingWebhookConfiguration{}
-							err := wcClient.Get(context.Background(), client.ObjectKey{Name: config.name}, mutatingWebhook)
-							return err == nil
-
-						case "ValidatingWebhookConfiguration":
-							validatingWebhook := &admissionregistrationv1.ValidatingWebhookConfiguration{}
-							err := wcClient.Get(context.Background(), client.ObjectKey{Name: config.name}, validatingWebhook)
-							return err == nil
+				for _, deploymentName := range deployments {
+					By(fmt.Sprintf("Checking deployment %s", deploymentName))
+					Eventually(func() error {
+						deployment := &appsv1.Deployment{}
+						err := wcClient.Get(context.Background(), types.NamespacedName{
+							Name:      deploymentName,
+							Namespace: namespace,
+						}, deployment)
+						if err != nil {
+							return err
 						}
 
-						return ready == replicas && replicas > 0
+						if deployment.Status.AvailableReplicas < 1 {
+							return fmt.Errorf("deployment %s not ready", deploymentName)
+						}
+						return nil
 					}).
-						WithTimeout(appReadyTimeout).
-						WithPolling(appReadyInterval).
-						Should(BeTrue(), fmt.Sprintf("%s %s should be ready", component, config.kind))
+						WithTimeout(deploymentReadyTimeout).
+						WithPolling(retryInterval).
+						ShouldNot(HaveOccurred())
 				}
 			})
+
+			It("should create required ServiceAccounts", func() {
+				wcClient := state.GetFramework().MC()
+
+				serviceAccounts := []string{
+					"cert-manager-app",
+					"cert-manager-app-webhook",
+					"cert-manager-app-cainjector",
+				}
+
+				for _, saName := range serviceAccounts {
+					By(fmt.Sprintf("Checking ServiceAccount %s", saName))
+					Eventually(func() error {
+						sa := &corev1.ServiceAccount{}
+						err := wcClient.Get(context.Background(), types.NamespacedName{
+							Name:      saName,
+							Namespace: namespace,
+						}, sa)
+						return err
+					}).
+						WithTimeout(testTimeout).
+						WithPolling(retryInterval).
+						ShouldNot(HaveOccurred())
+				}
+			})
+
+			It("should create ClusterRoles and ClusterRoleBindings", func() {
+				wcClient := state.GetFramework().MC()
+
+				clusterRoles := []string{
+					"cert-manager-app-controller-issuers",
+					"cert-manager-app-controller-clusterissuers",
+					"cert-manager-app-controller-certificates",
+					"cert-manager-app-controller-orders",
+					"cert-manager-app-controller-challenges",
+					"cert-manager-app-controller-ingress-shim",
+					"cert-manager-app-cainjector",
+					"cert-manager-app-webhook:subjectaccessreviews",
+				}
+
+				for _, roleName := range clusterRoles {
+					By(fmt.Sprintf("Checking ClusterRole %s", roleName))
+					Eventually(func() error {
+						role := &rbacv1.ClusterRole{}
+						err := wcClient.Get(context.Background(), types.NamespacedName{Name: roleName}, role)
+						return err
+					}).
+						WithTimeout(testTimeout).
+						WithPolling(retryInterval).
+						ShouldNot(HaveOccurred())
+
+					By(fmt.Sprintf("Checking ClusterRoleBinding %s", roleName))
+					Eventually(func() error {
+						binding := &rbacv1.ClusterRoleBinding{}
+						err := wcClient.Get(context.Background(), types.NamespacedName{Name: roleName}, binding)
+						return err
+					}).
+						WithTimeout(testTimeout).
+						WithPolling(retryInterval).
+						ShouldNot(HaveOccurred())
+				}
+			})
+
+			It("should create default ClusterIssuers", func() {
+				wcClient := state.GetFramework().MC()
+
+				issuers := []string{
+					"letsencrypt-giantswarm",
+					"selfsigned-giantswarm",
+				}
+
+				for _, issuerName := range issuers {
+					By(fmt.Sprintf("Checking ClusterIssuer %s", issuerName))
+					Eventually(func() error {
+						issuer := &cmapi.ClusterIssuer{}
+						err := wcClient.Get(context.Background(), types.NamespacedName{Name: issuerName}, issuer)
+						if err != nil {
+							return err
+						}
+
+						// Check if issuer is ready
+						for _, condition := range issuer.Status.Conditions {
+							if condition.Type == "Ready" {
+								if condition.Status == "True" {
+									return nil
+								}
+								return fmt.Errorf("issuer %s not ready: %s", issuerName, condition.Message)
+							}
+						}
+						return errors.New("ready condition not found")
+					}).
+						WithTimeout(testTimeout).
+						WithPolling(retryInterval).
+						ShouldNot(HaveOccurred())
+				}
+			})
+
+			It("should create test certificate with self-signed issuer", func() {
+				wcClient := state.GetFramework().MC()
+
+				By("Creating test certificate")
+				cert := &cmapiv1.Certificate{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-cert",
+						Namespace: namespace,
+					},
+					Spec: cmapiv1.CertificateSpec{
+						SecretName: "test-cert-tls",
+						IssuerRef: cmmeta.ObjectReference{
+							Name:  "selfsigned-giantswarm",
+							Kind:  "ClusterIssuer",
+							Group: "cert-manager.io",
+						},
+						CommonName: "test.local",
+						DNSNames:   []string{"test.local"},
+					},
+				}
+
+				err := wcClient.Create(context.Background(), cert)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				By("Waiting for certificate to be ready")
+				Eventually(func() error {
+					cert := &cmapi.Certificate{}
+					err := wcClient.Get(context.Background(), types.NamespacedName{
+						Name:      "test-cert",
+						Namespace: namespace,
+					}, cert)
+					if err != nil {
+						return err
+					}
+
+					for _, condition := range cert.Status.Conditions {
+						if condition.Type == "Ready" {
+							if condition.Status == "True" {
+								return nil
+							}
+							return fmt.Errorf("certificate not ready: %s", condition.Message)
+						}
+					}
+					return errors.New("ready condition not found")
+				}).
+					WithTimeout(testTimeout).
+					WithPolling(retryInterval).
+					ShouldNot(HaveOccurred())
+
+				By("Verifying the certificate secret was created")
+				Eventually(func() error {
+					secret := &corev1.Secret{}
+					err := wcClient.Get(context.Background(), types.NamespacedName{
+						Name:      "test-cert-tls",
+						Namespace: namespace,
+					}, secret)
+					return err
+				}).
+					WithTimeout(testTimeout).
+					WithPolling(retryInterval).
+					ShouldNot(HaveOccurred())
+			})
+		}).
+		AfterSuite(func() {
+			wcClient := state.GetFramework().MC()
+
+			By("Cleaning up test certificate")
+			cert := &cmapi.Certificate{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-cert",
+					Namespace: namespace,
+				},
+			}
+			err := wcClient.Delete(context.Background(), cert)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			By("Cleaning up test certificate secret")
+			secret := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-cert-tls",
+					Namespace: namespace,
+				},
+			}
+			err = wcClient.Delete(context.Background(), secret)
+			Expect(err).ShouldNot(HaveOccurred())
 		}).
 		Run(t, "Basic Test")
 }
