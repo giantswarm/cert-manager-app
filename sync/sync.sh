fee2d2e4922c970c9b8871274b4247a694821855
Spyros Synodinos <spyros@giantswarm.io>
2025-01-14 18:54:33 +0200

    pss patch

--- sync/sync.sh
+++ sync/sync.sh
@@ -19,6 +19,7 @@ vendir sync
 ./sync/patches/values/patch.sh
 ./sync/patches/chart/patch.sh
 ./sync/patches/image-registry/patch.sh
+./sync/patches/pss/patch.sh
 
 HELM_DOCS="docker run --rm -u $(id -u) -v ${PWD}:/helm-docs -w /helm-docs jnorwood/helm-docs:v1.11.0"
 $HELM_DOCS --template-files=sync/readme.gotmpl -g helm/cert-manager -f values.yaml -o README.md
