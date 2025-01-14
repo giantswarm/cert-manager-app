78bcb557c67e95b8f561a363261fefb92e5799ef
Spyros Synodinos <spyros@giantswarm.io>
2025-01-14 18:40:20 +0200

    image-registry patch

---
+++ sync/patches/image-registry/patch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+repo_dir=$(git rev-parse --show-toplevel) ; readonly repo_dir
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly script_dir
+
+cd "${repo_dir}"
+
+readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
+
+set -x
+git apply "${script_dir_rel}/000-image-registry.patch"
+
+{ set +x; } 2>/dev/null
--- sync/sync.sh
+++ sync/sync.sh
@@ -18,6 +18,7 @@ vendir sync
 # Patches
 ./sync/patches/values/patch.sh
 ./sync/patches/chart/patch.sh
+./sync/patches/image-registry/patch.sh
 
 HELM_DOCS="docker run --rm -u $(id -u) -v ${PWD}:/helm-docs -w /helm-docs jnorwood/helm-docs:v1.11.0"
 $HELM_DOCS --template-files=sync/readme.gotmpl -g helm/cert-manager -f values.yaml -o README.md
