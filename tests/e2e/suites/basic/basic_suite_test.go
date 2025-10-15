3a559a22a520f6b0bfe46370e631adfc9bd5a8c0
Spyros Synodinos <spyros@giantswarm.io>
2025-10-15 15:14:31 +0300

    further simplify

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -2,6 +2,8 @@ package basic
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 	"time"
 
@@ -12,10 +14,11 @@ import (
 	"github.com/giantswarm/apptest-framework/pkg/suite"
 	"github.com/giantswarm/clustertest/pkg/logger"
 
-	corev1 "k8s.io/api/core/v1"
+	helmv2beta2 "github.com/fluxcd/helm-controller/api/v2beta2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
@@ -28,18 +31,50 @@ func TestBasic(t *testing.T) {
 		WithIsUpgrade(isUpgrade).
 		WithValuesFile("./values.yaml").
 		AfterClusterReady(func() {
-			// Hook for actions after cluster is ready
+			// no
 		}).
 		Tests(func() {
-			It("should have all required deployments ready", func() {
+			It("should deploy the HelmRelease", func() {
+				Eventually(func() (bool, error) {
+					appNamespace := state.GetCluster().Organization.GetNamespace()
+					appName := fmt.Sprintf("%s-cert-manager-app", state.GetCluster().Name)
+
+					mcKubeClient := state.GetFramework().MC()
+
+					logger.Log("HelmRelease: %s/%s", appNamespace, appName)
+
+					release := &helmv2beta2.HelmRelease{}
+					err := mcKubeClient.Get(state.GetContext(), types.NamespacedName{Name: appName, Namespace: appNamespace}, release)
+					if err != nil {
+						return false, err
+					}
+
+					for _, c := range release.Status.Conditions {
+						if c.Type == "Ready" {
+							if c.Status == "True" {
+								return true, nil
+							} else {
+								return false, errors.New(fmt.Sprintf("HelmRelease not ready [%s]: %s", c.Reason, c.Message))
+							}
+						}
+					}
+
+					return false, errors.New("HelmRelease not ready")
+				}).
+					WithTimeout(10 * time.Minute).
+					WithPolling(15 * time.Second).
+					Should(BeTrue())
+			})
+
+			It("should have all cert-manager deployments ready", func() {
+				wcClient, _ := state.GetFramework().WC(state.GetCluster().Name)
+				appNamespace := "default"
+
 				deploymentNames := []string{
 					"cert-manager-app",
 					"cert-manager-app-cainjector",
 					"cert-manager-app-webhook",
 				}
-
-				wcClient, _ := state.GetFramework().WC(state.GetCluster().Name)
-				appNamespace := "default"
 
 				for _, deploymentName := range deploymentNames {
 					logger.Log("Checking deployment: %s/%s", appNamespace, deploymentName)
@@ -50,16 +85,14 @@ func TestBasic(t *testing.T) {
 							return 0, err
 						}
 						return deployment.Status.ReadyReplicas, nil
-					}, 10*time.Minute, 15*time.Second).Should(BeNumerically(">", 0))
+					}).
+						WithTimeout(10 * time.Minute).
+						WithPolling(15 * time.Second).
+						Should(BeNumerically(">", 0))
 				}
 			})
 
-			It("should have all required cluster issuers created", func() {
-				expectedIssuers := []string{
-					"letsencrypt-giantswarm",
-					"selfsigned-giantswarm",
-				}
-
+			It("should have cluster issuers ready", func() {
 				wcClient, _ := state.GetFramework().WC(state.GetCluster().Name)
 
 				clusterIssuerGVR := schema.GroupVersionResource{
@@ -68,38 +101,52 @@ func TestBasic(t *testing.T) {
 					Resource: "clusterissuers",
 				}
 
+				expectedIssuers := []string{
+					"letsencrypt-giantswarm",
+					"selfsigned-giantswarm",
+				}
+
 				for _, issuerName := range expectedIssuers {
 					logger.Log("Checking ClusterIssuer: %s", issuerName)
 
-					Eventually(func() error {
-						_, err := wcClient.Dynamic().Resource(clusterIssuerGVR).Get(context.Background(), issuerName, metav1.GetOptions{})
-						return err
-					}, 5*time.Minute, 15*time.Second).Should(Succeed())
+					Eventually(func() (bool, error) {
+						issuer, err := wcClient.Dynamic().Resource(clusterIssuerGVR).Get(context.Background(), issuerName, metav1.GetOptions{})
+						if err != nil {
+							return false, err
+						}
+
+						// Check if ready
+						conditions, found, err := unstructured.NestedSlice(issuer.Object, "status", "conditions")
+						if err != nil || !found {
+							return false, nil
+						}
+
+						for _, condition := range conditions {
+							if conditionMap, ok := condition.(map[string]interface{}); ok {
+								if conditionMap["type"] == "Ready" && conditionMap["status"] == "True" {
+									return true, nil
+								}
+							}
+						}
+						return false, nil
+					}).
+						WithTimeout(5 * time.Minute).
+						WithPolling(15 * time.Second).
+						Should(BeTrue())
 				}
 			})
 
-			It("should have cert-manager pods running", func() {
+			It("should issue a self-signed certificate", func() {
 				wcClient, _ := state.GetFramework().WC(state.GetCluster().Name)
 				appNamespace := "default"
+				testCertName := "test-cert"
+				testSecretName := "test-cert-secret"
 
-				logger.Log("Checking cert-manager pods in namespace: %s", appNamespace)
-
-				pods, err := wcClient.CoreV1().Pods(appNamespace).List(context.Background(), metav1.ListOptions{
-					LabelSelector: "app.kubernetes.io/name=cert-manager",
-				})
-				Expect(err).ToNot(HaveOccurred())
-				Expect(len(pods.Items)).To(BeNumerically(">=", 1))
-
-				for _, pod := range pods.Items {
-					Expect(pod.Status.Phase).To(Equal(corev1.PodPhase("Running")))
+				certificateGVR := schema.GroupVersionResource{
+					Group:    "cert-manager.io",
+					Version:  "v1",
+					Resource: "certificates",
 				}
-			})
-
-			It("should be able to issue a self-signed certificate", func() {
-				wcClient, _ := state.GetFramework().WC(state.GetCluster().Name)
-				appNamespace := "default"
-				testCertName := "test-selfsigned-cert"
-				testSecretName := "test-selfsigned-secret"
 
 				By("Creating a test certificate")
 				certificate := &unstructured.Unstructured{
@@ -116,16 +163,10 @@ func TestBasic(t *testing.T) {
 								"name": "selfsigned-giantswarm",
 								"kind": "ClusterIssuer",
 							},
-							"commonName": "test.example.com",
-							"dnsNames":   []string{"test.example.com", "www.test.example.com"},
+							"commonName": "test.giantswarm.io",
+							"dnsNames":   []string{"test.giantswarm.io"},
 						},
 					},
-				}
-
-				certificateGVR := schema.GroupVersionResource{
-					Group:    "cert-manager.io",
-					Version:  "v1",
-					Resource: "certificates",
 				}
 
 				_, err := wcClient.Dynamic().Resource(certificateGVR).Namespace(appNamespace).Create(context.Background(), certificate, metav1.CreateOptions{})
@@ -151,18 +192,36 @@ func TestBasic(t *testing.T) {
 						}
 					}
 					return false, nil
-				}, 3*time.Minute, 10*time.Second).Should(BeTrue())
+				}).
+					WithTimeout(3 * time.Minute).
+					WithPolling(10 * time.Second).
+					Should(BeTrue())
 
 				By("Verifying the secret was created")
 				Eventually(func() error {
 					_, err := wcClient.CoreV1().Secrets(appNamespace).Get(context.Background(), testSecretName, metav1.GetOptions{})
 					return err
-				}, 1*time.Minute, 5*time.Second).Should(Succeed())
-
-				By("Cleaning up test resources")
-				_ = wcClient.Dynamic().Resource(certificateGVR).Namespace(appNamespace).Delete(context.Background(), testCertName, metav1.DeleteOptions{})
-				_ = wcClient.CoreV1().Secrets(appNamespace).Delete(context.Background(), testSecretName, metav1.DeleteOptions{})
+				}).
+					WithTimeout(1 * time.Minute).
+					WithPolling(5 * time.Second).
+					Should(Succeed())
 			})
 		}).
-		Run(t, "cert-manager-app Basic Suite")
+		AfterSuite(func() {
+			wcClient, _ := state.GetFramework().WC(state.GetCluster().Name)
+			appNamespace := "default"
+
+			certificateGVR := schema.GroupVersionResource{
+				Group:    "cert-manager.io",
+				Version:  "v1",
+				Resource: "certificates",
+			}
+
+			By("Cleaning up test certificate")
+			_ = wcClient.Dynamic().Resource(certificateGVR).Namespace(appNamespace).Delete(context.Background(), "test-cert", metav1.DeleteOptions{})
+
+			By("Cleaning up test secret")
+			_ = wcClient.CoreV1().Secrets(appNamespace).Delete(context.Background(), "test-cert-secret", metav1.DeleteOptions{})
+		}).
+		Run(t, "Basic Test")
 }
