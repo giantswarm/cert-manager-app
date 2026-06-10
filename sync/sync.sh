42a3b0a9f8b1b8835830537e520db1677ad02da6
Franco <franco@giantswarm.io>
2026-06-10 10:12:31 -0300

    chore(patches): migrate patches to new structure

--- sync/sync.sh
+++ sync/sync.sh
@@ -15,20 +15,22 @@ vendir sync
 # Remove trailing whitespace end of lines (hack to fix vendir bug)
 # find vendor/ -type f -exec sed -i 's/[[:space:]]*$//' {} \;
 
-# Patches
-./sync/patches/values/patch.sh
-./sync/patches/chart/patch.sh
+# Patches. vendir syncs the pristine upstream chart into
+# helm/cert-manager/charts/cert-manager; these patches only tweak upstream-rendered
+# objects that cannot live in the parent chart's templates.
 ./sync/patches/image-registry/patch.sh
 ./sync/patches/webhook-pdb/patch.sh
 ./sync/patches/cainjector-service/patch.sh
 
-HELM_DOCS="docker run --rm -u $(id -u) -v ${PWD}:/helm-docs -w /helm-docs jnorwood/helm-docs:v1.11.0"
-$HELM_DOCS --template-files=sync/readme.gotmpl -g helm/cert-manager -f values.yaml -o README.md
+# Giant Swarm subcharts (clusterissuer, netpol, ciliumnetworkpolicies) live alongside
+# the vendored cert-manager subchart.
+cp -R ./sync/charts/* ./helm/cert-manager/charts/
 
-# Store diffs
-rm -f ./diffs/*
+# Giant-Swarm-only templates and the _gs-helpers.tpl overlay go into the parent chart.
+cp -R ./sync/templates/* ./helm/cert-manager/templates/
 
-cp -R ./sync/charts ./helm/cert-manager
-cp -R ./sync/templates/* ./helm/cert-manager/templates
 cp  ./sync/.kube-linter.yaml ./helm/cert-manager/.kube-linter.yaml
 cp  ./sync/.helmignore ./helm/cert-manager/.helmignore
+
+HELM_DOCS="docker run --rm -u $(id -u) -v ${PWD}:/helm-docs -w /helm-docs jnorwood/helm-docs:v1.11.0"
+$HELM_DOCS --template-files=sync/readme.gotmpl -g helm/cert-manager -f values.yaml -o README.md
